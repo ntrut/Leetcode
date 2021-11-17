eccff162e7e999468d00528e9e099aa54e5d0493
ntrut <71266285+ntrut@users.noreply.github.com>
2021-11-16 19:56:35 -0500

    Found better solution

--- 268.MissingNumber/main.java
+++ 268.MissingNumber/main.java
@@ -1,25 +1,19 @@
 class Solution {
     public int missingNumber(int[] nums) 
     {
-       int len = nums.length + 1;
-        HashMap<Integer,Boolean> hashmap = new HashMap<>();
+        int total = 0;
+        int sum = 0;
+        
+        for(int i = 0; i < nums.length+1; i++)
+        {
+            total = total + i;
+        }
         
         for(int i = 0; i < nums.length; i++)
         {
-            if(!hashmap.containsKey(nums[i]))
-            {
-                hashmap.put(nums[i], true);
-            }
+            sum = sum + nums[i];
         }
         
-        for(int i = 0; i <= len; i++)
-        {
-            if(!hashmap.containsKey(i))
-            {
-                return i;
-            }
-        }
-        
-        return -1;
+        return total - sum;
     }
 }
